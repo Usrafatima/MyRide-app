126bd851145943486410ead0a0219e463aa9b9e3
Yusra Fatima <162156870+Usrafatima@users.noreply.github.com>
2025-03-28 01:49:21 +0500

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,8 +1,6 @@
-// src/types.ts
 export interface Place {
-    lat: number;
-    lng: number;
-    osm_id?: number; // Optional if not always present
-    display_name?: string; // Optional if not always present
-  }
-  
+  lat: number;
+  lng: number;
+  osm_id?: number; // ✅ Now optional
+  display_name?: string;
+}
