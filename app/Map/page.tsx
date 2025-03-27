cfe37639ae977e1a4b9ba9b2303838601088a9ce
Yusra Fatima <162156870+Usrafatima@users.noreply.github.com>
2025-03-28 00:58:40 +0500

    Update page.tsx

--- app/Map/page.tsx
+++ app/Map/page.tsx
@@ -20,13 +20,13 @@ export default function MapBoxMap() {
       <div className="flex flex-col items-center w-full h-[500px] bg-[#242424]">
         {/* Left Side - Location Search */}
         <div className="w-full lg:w-1/2 h-full mt-2">
-          <Map
-            selectPosition={
-              selectPosition
-                ? ([selectPosition.lat, selectPosition.lng] as LatLngExpression)
-                : undefined
-            }
-          />
+         <Map
+  selectPosition={
+    selectPosition
+      ? { lat: selectPosition.lat, lon: selectPosition.lng } // ✅ Use 'lon' instead of 'lng'
+      : undefined
+  }
+/>
         </div>
         {/* Right Side - Location Inputs */}
         <div className="w-1/2 p-4 border-r-2">
