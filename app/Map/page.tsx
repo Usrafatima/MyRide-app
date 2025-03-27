a2e62132f837573578f2756d109525680916bbdc
Yusra Fatima <162156870+Usrafatima@users.noreply.github.com>
2025-03-27 21:21:04 +0500

    Update page.tsx

--- app/Map/page.tsx
+++ app/Map/page.tsx
@@ -5,28 +5,27 @@ import LocationSearch from '../components/LocationSearch';
 
 import Dropoff from "@/app/components/Dropoff";
 
-
-
+interface Place {
+  lat: number;
+  lng: number;
+}
 
 export default function MapBoxMap() {
-  const [selectPosition, setSelectPosition] = useState(null);
-
-
+  const [selectPosition, setSelectPosition] = useState<Place | null>(null);
 
   return (
-    <div className=''>
-    <div className="flex flex-col items-center w-full h-[500px]  border-red bg-[#242424] ">
-      {/* Left Side - Location Search */}
-      <div className="w-full  lg:w-1/2 h-full mt-2">
-        <Map selectPosition={selectPosition ?? undefined} />
+    <div className="">
+      <div className="flex flex-col items-center w-full h-[500px] bg-[#242424]">
+        {/* Left Side - Location Search */}
+        <div className="w-full lg:w-1/2 h-full mt-2">
+          <Map selectPosition={selectPosition ?? undefined} />
+        </div>
+        {/* Right Side - Location Inputs */}
+        <div className="w-1/2 p-4 border-r-2">
+          <LocationSearch setSelectPosition={(place) => setSelectPosition(place)} selectPosition={null} />
+          <Dropoff setSelectPosition={(place) => setSelectPosition(place)} />
+        </div>
       </div>
-      {/* Right Side - Map */}
-      <div className="w-1/2 p-4 border-r-2">
-<LocationSearch selectPosition={selectPosition} setSelectPosition={setSelectPosition} />
-<Dropoff selectPosition={selectPosition} setSelectPosition={setSelectPosition} />
-</div>
-
-    </div>
     </div>
   );
 }
